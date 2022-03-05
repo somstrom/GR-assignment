14779587ab7608333f6bb6f0ecfef3eaf07f3749
Marek Lukáč <marek.lukac@student.tuke.sk>
2022-03-05 18:42:55 +0100

    Pageui paragraph wrapper added

---
+++ src/components/PageUI/ParagraphWrapper.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledPWrapper } from "./StyledParagraphWrapper";
+
+type props = {
+  children: React.ReactNode;
+};
+
+const ParagraphWrapper = ({ children }: props) => {
+  return <StyledPWrapper>{children}</StyledPWrapper>;
+};
+
+export default ParagraphWrapper;
