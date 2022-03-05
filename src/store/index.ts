18737d84b7c3d966c060b10794cedad465a6b22c
Marek Lukáč <marek.lukac@student.tuke.sk>
2022-03-05 22:46:54 +0100

    Data persisted after refresh

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,16 @@ import { createStore, compose } from "redux";
 import allReducer from "./reducers";
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(allReducer, composeWithDevTools());
+const persistedState = localStorage.getItem('reduxState') 
+                       ? JSON.parse(localStorage.getItem('reduxState') || '')
+                       : {}
+
+const store = createStore(allReducer, persistedState ,composeWithDevTools());
+
+store.subscribe(()=>{
+    localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+  })
+
+
 
 export default store;
