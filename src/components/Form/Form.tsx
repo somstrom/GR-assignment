296b436990fd3291841beb4f8116c4510d9be4df
Marek Lukáč <marek.lukac@student.tuke.sk>
2022-03-05 18:47:38 +0100

    FormParagraph changed to PageParagraph in form and onsubmit handler added

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+import PageParagraph from "../PageUI/PageParagraph/PageParagraph";
 
 import { FormParagraph, FormWrapper } from "./StyledForm";
 
 type props = {
   children: React.ReactNode;
   title: string;
+  onSubmit: any
 };
 
-const Form = ({ children, title }: props) => {
+const Form = ({ children, title, onSubmit }: props) => {
   return (
-    <FormWrapper>
-      <FormParagraph>{title}</FormParagraph>
+    <FormWrapper onSubmit={onSubmit}>
+      <PageParagraph titleParagraph={title}></PageParagraph>
       {children}
     </FormWrapper>
   );
