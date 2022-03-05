b4eb13e0903159761afd65e143961bf8cf23fe90
Marek Lukáč <marek.lukac@student.tuke.sk>
2022-03-05 18:52:46 +0100

    Static data replaced with data from redux store

--- src/containers/FinalPage/FinalPage.tsx
+++ src/containers/FinalPage/FinalPage.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 import CheckBox from "../../components/Form/CheckBox/CheckBox";
 import Header from "../../components/Header/Header";
@@ -10,11 +12,73 @@ import PageContent from "../../components/PageContent/PageContent";
 import PageTitle from "../../components/PageUI/PageTitle/PageTitle";
 import SecondaryButton from "../../components/Buttons/Secondary/SecondaryButton";
 import PrimaryButton from "../../components/Buttons/Primary/PrimaryButton";
+import StyledContainerBtn from "../../components/Buttons/ButtonsContainer";
 import ButtonsContainer from "../../components/Buttons/ButtonsContainer";
 
+type flag = {
+  src: string;
+  prefix: string;
+  alt: string;
+};
 
+type FormData = {
+  firstname?: string;
+  lastname: string;
+  email?: string;
+  phone?: number;
+};
+
+type button = {
+  value: number;
+  isActive: boolean;
+};
+
+type action = {
+  paragraph: string;
+  isActive: boolean;
+  src: string;
+  type: string;
+  id: number;
+};
+
+
+type shelter = {
+  id: number;
+  name: string;
+};
 
 const FinalPage = () => {
+
+  const [checked, setChecked] = useState<boolean>(true);
+
+
+  const formData: FormData =  useSelector(
+    (state: any) => state.formReducer
+  );
+
+  const activeFlag: flag = useSelector(
+    (state: any) => state.activeFlag
+  )
+
+  const moneyValue: number = useSelector(
+    (state: any) => state.moneyButtons.find((button: button) => button.isActive).value
+  )
+
+  const actionValue: string = useSelector(
+    (state: any) => state.actionButtons.find((button: action) => button.isActive).paragraph
+  )
+
+  const shelter: shelter = useSelector(
+    (state: any) => state.chosenShelter
+  )
+
+  const toggleChecked = () =>{
+    setChecked(!checked)
+  }
+
+
+  useEffect(() => {console.log(actionValue)},[])
+
   return (
     <>
       <Header />
@@ -22,30 +86,48 @@ const FinalPage = () => {
         <PageContent>
           <PageIndicator activePage={[false, false, true]} />
           <PageTitle title={"Skontrolujte si zadané údaje"} />
-            <PageParagraph
-              infoParagraph={"Chcem finančne prispieť celej nadácií"}
-              titleParagraph={"Akou formou chem pomôcť"} />
-            <PageParagraph
-              infoParagraph={"Mestský útulok, Žilina"}
-              titleParagraph={"Najviac mi záleží na útulku"} />
-            <PageParagraph
-              titleParagraph={"Suma ktorou chcem pomôcť"}
-              infoParagraph={"50 €"} />
-            <PageParagraph
-              titleParagraph={"Meno a priezvisko"}
-              infoParagraph={"Peter Reguli"} />
-            <PageParagraph
-              titleParagraph={"E-mailová adresa"}
-              infoParagraph={"peter.reguli@goodrequest.com"} />
-            <PageParagraph
-              titleParagraph={"Telefónne číslo"}
-              infoParagraph={"+421 902 237 207"} />
-            <CheckBox id={'final-page-check-box'} label={'Súhlasím so spracovaním mojich osobných údajov'} isRequired={true} />
-            <ButtonsContainer>
+          <PageParagraph
+            infoParagraph={actionValue}
+            titleParagraph={"Akou formou chem pomôcť"}
+          />{shelter && (
+          <PageParagraph
+            infoParagraph={shelter.name}
+            titleParagraph={"Najviac mi záleží na útulku"}
+          />)}
+          <PageParagraph
+            titleParagraph={"Suma ktorou chcem pomôcť"}
+            infoParagraph={`${moneyValue} €`}
+          />
+          <PageParagraph
+            titleParagraph={"Meno a priezvisko"}
+            infoParagraph={`${formData.firstname} ${formData.lastname}`}
+          />
+          {formData.email && (
+          <PageParagraph
+            titleParagraph={"E-mailová adresa"}
+            infoParagraph={formData.email}
+          />
+          )}
+          {formData.phone && (
+          <PageParagraph
+            titleParagraph={"Telefónne číslo"}
+            infoParagraph={`${activeFlag.prefix} ${formData.phone}`}
+          />
+          )}
+          <CheckBox
+            handleCheck={toggleChecked}
+            id={"final-page-check-box"}
+            label={"Súhlasím so spracovaním mojich osobných údajov"}
+            isRequired={true}
+          />
+          <ButtonsContainer >
+            <Link to="/form">
               <SecondaryButton context={"Späť"} />
-              <PrimaryButton context={'Odoslať formulár'} disabled={true} />
-            </ButtonsContainer>
-          </PageContent><PageImage />
+            </Link>
+            <PrimaryButton context={"Odoslať formulár"} disabled={checked} />
+          </ButtonsContainer>
+        </PageContent>
+        <PageImage />
       </Container>
     </>
   );
