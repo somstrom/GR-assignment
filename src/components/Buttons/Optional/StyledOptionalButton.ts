122335384ab0d3627e6c9cf4459e04946b611d1e
Marek Lukáč <marek.lukac@student.tuke.sk>
2022-03-05 22:50:35 +0100

    Optional button borders bug fixed

--- src/components/Buttons/Optional/StyledOptionalButton.ts
+++ src/components/Buttons/Optional/StyledOptionalButton.ts
@@ -45,6 +45,7 @@ cursor: pointer;
   justify-content: space-between;
   align-items: start;
   padding: 1.5rem;
+  border: ${(props) => props.isActive ? 'none;' : '1px solid #CD8B65;'}
   color: ${(props) => (props.isActive ? "white;" : "#585757;")}
   background:  ${(props) =>
     props.isActive
@@ -52,7 +53,7 @@ cursor: pointer;
       : "#faf9f9;"}
   box-shadow: ${(props) =>
     props.isActive
-      ? "box-shadow: 0px 100px 80px rgba(0, 0, 0, 0.07),0px 41.7776px 33.4221px rgba(0, 0, 0, 0.0503198), 0px 22.3363px 17.869px rgba(0, 0, 0, 0.0417275), 0px 12.5216px 10.0172px rgba(0, 0, 0, 0.035),0px 6.6501px 5.32008px rgba(0, 0, 0, 0.0282725), 0px 2.76726px 2.21381px rgba(0, 0, 0, 0.0196802);"
+      ? "0px 100px 80px rgba(0, 0, 0, 0.07),0px 41.7776px 33.4221px rgba(0, 0, 0, 0.0503198), 0px 22.3363px 17.869px rgba(0, 0, 0, 0.0417275), 0px 12.5216px 10.0172px rgba(0, 0, 0, 0.035),0px 6.6501px 5.32008px rgba(0, 0, 0, 0.0282725), 0px 2.76726px 2.21381px rgba(0, 0, 0, 0.0196802);"
       : "none;"}
   &:focus ${OptionalButtonIcon} {
     color: white;
