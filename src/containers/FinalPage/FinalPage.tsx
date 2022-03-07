e82c89b87d3bf617bb4b8e0aa39efd9ff3e5e341
Marek Lukáč <marek.lukac@student.tuke.sk>
2022-03-07 11:46:41 +0100

    Added POST, small changes in types and conditions

--- src/containers/FinalPage/FinalPage.tsx
+++ src/containers/FinalPage/FinalPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
 
 import CheckBox from "../../components/Form/CheckBox/CheckBox";
 import Header from "../../components/Header/Header";
@@ -14,6 +15,7 @@ import SecondaryButton from "../../components/Buttons/Secondary/SecondaryButton"
 import PrimaryButton from "../../components/Buttons/Primary/PrimaryButton";
 import ButtonsContainer from "../../components/Buttons/ButtonsContainer";
 import { RESET_ACTION, SET_ACCESSIBLE_PAGES } from "../../store/actions";
+import { valueTernary } from "react-select/dist/declarations/src/utils";
 
 type flag = {
   src: string;
@@ -24,13 +26,14 @@ type flag = {
 type FormData = {
   firstname?: string;
   lastname: string;
-  email?: string;
-  phone?: number;
+  email: string;
+  phone?: string;
 };
 
 type button = {
   value: number;
   isActive: boolean;
+  type?: string;
 };
 
 type action = {
@@ -57,7 +60,7 @@ const FinalPage = () => {
 
   const moneyValue: number = useSelector(
     (state: any) =>
-      state.moneyButtons.find((button: button) => button.isActive).value
+      state.moneyButtons.find((button: button) => button.isActive).value || 0
   );
 
   const actionValue: string = useSelector(
@@ -76,6 +79,7 @@ const FinalPage = () => {
   };
 
   const handleClick = () => {
+    handlePost();
     dispatch(RESET_ACTION);
     dispatch(SET_ACCESSIBLE_PAGES([true, false, false]));
   };
@@ -84,64 +88,99 @@ const FinalPage = () => {
     !accessiblePages[2] && history.goBack();
   }, []);
 
+  const handlePost = () => {
+    console.log({
+      ...(formData.firstname
+        ? { firstName: formData.firstname }
+        : { firstName: " " }),
+      lastName: formData.lastname,
+      email: formData.email,
+      value: moneyValue,
+      ...(formData.phone && {
+        phone: activeFlag.prefix + formData.phone.split(" ").join(),
+      }),
+      ...(shelter.id !== -1 && { shelterID: shelter.id }),
+    });
+    axios
+      .post(
+        "https://frontend-assignment-api.goodrequest.dev/api/v1/shelters/contribute",
+        {
+          firstName: formData.firstname || " ",
+          lastName: formData.lastname,
+          email: formData.email,
+          value: moneyValue,
+          ...(formData.phone && {
+            phone: activeFlag.prefix + formData.phone.split(" ").join(''),
+          }),
+          ...(shelter.id !== -1 && { shelterID: shelter.id }),
+        }
+      )
+      .then((response: any) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <Header />
-        <Container>
-          <PageContent>
-            <PageIndicator activePage={[false, false, true]} />
-            <PageTitle title={"Skontrolujte si zadané údaje"} />
+      <Container>
+        <PageContent>
+          <PageIndicator activePage={[false, false, true]} />
+          <PageTitle title={"Skontrolujte si zadané údaje"} />
+          <PageParagraph
+            infoParagraph={actionValue}
+            titleParagraph={"Akou formou chem pomôcť"}
+          />
+          {shelter.name !== "empty" && (
             <PageParagraph
-              infoParagraph={actionValue}
-              titleParagraph={"Akou formou chem pomôcť"}
+              infoParagraph={shelter.name}
+              titleParagraph={"Najviac mi záleží na útulku"}
             />
-            {shelter.name !== 'empty' && (
-              <PageParagraph
-                infoParagraph={shelter.name}
-                titleParagraph={"Najviac mi záleží na útulku"}
-              />
-            )}
+          )}
+          <PageParagraph
+            titleParagraph={"Suma ktorou chcem pomôcť"}
+            infoParagraph={`${moneyValue} €`}
+          />
+          <PageParagraph
+            titleParagraph={"Meno a priezvisko"}
+            infoParagraph={`${formData.firstname} ${formData.lastname}`}
+          />
+          {formData.email && (
             <PageParagraph
-              titleParagraph={"Suma ktorou chcem pomôcť"}
-              infoParagraph={`${moneyValue} €`}
+              titleParagraph={"E-mailová adresa"}
+              infoParagraph={formData.email}
             />
+          )}
+          {formData.phone && (
             <PageParagraph
-              titleParagraph={"Meno a priezvisko"}
-              infoParagraph={`${formData.firstname} ${formData.lastname}`}
+              titleParagraph={"Telefónne číslo"}
+              infoParagraph={`${activeFlag.prefix} ${formData.phone}`}
             />
-            {formData.email && (
-              <PageParagraph
-                titleParagraph={"E-mailová adresa"}
-                infoParagraph={formData.email}
+          )}
+          <CheckBox
+            handleCheck={toggleChecked}
+            id={"final-page-check-box"}
+            label={"Súhlasím so spracovaním mojich osobných údajov"}
+            isRequired={true}
+          />
+          <ButtonsContainer type="nav-buttons">
+            <Link to="/formular">
+              <SecondaryButton context={"Späť"} />
+            </Link>
+            <Link to="">
+              <PrimaryButton
+                context={"Odoslať formulár"}
+                disabled={checked}
+                onClick={handleClick}
               />
-            )}
-            {formData.phone && (
-              <PageParagraph
-                titleParagraph={"Telefónne číslo"}
-                infoParagraph={`${activeFlag.prefix} ${formData.phone}`}
-              />
-            )}
-            <CheckBox
-              handleCheck={toggleChecked}
-              id={"final-page-check-box"}
-              label={"Súhlasím so spracovaním mojich osobných údajov"}
-              isRequired={true}
-            />
-            <ButtonsContainer>
-              <Link to="/formular">
-                <SecondaryButton context={"Späť"} />
-              </Link>
-              <Link to="">
-                <PrimaryButton
-                  context={"Odoslať formulár"}
-                  disabled={checked}
-                  onClick={handleClick}
-                />
-              </Link>
-            </ButtonsContainer>
-          </PageContent>
-          <PageImage />
-        </Container>
+            </Link>
+          </ButtonsContainer>
+        </PageContent>
+        <PageImage />
+      </Container>
     </>
   );
 };
