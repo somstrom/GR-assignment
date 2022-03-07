112abda6eb729efaab29a0aafcb11554d4e15a3c
Marek Lukáč <marek.lukac@student.tuke.sk>
2022-03-07 11:48:32 +0100

    Reducer for money buttons updated

--- src/store/reducers/switchButton.ts
+++ src/store/reducers/switchButton.ts
@@ -1,30 +1,61 @@
+import { ButtonPrimary } from "../../components/Buttons/Primary/StyledPrimaryButton";
+
 type button = {
   value: number;
   isActive: boolean;
+  type?: string;
 };
 
 const initialState: button[] = [
-  { value: 5, isActive: true },
-  { value: 10, isActive: false },
-  { value: 20, isActive: false },
-  { value: 30, isActive: false },
-  { value: 50, isActive: false },
-  { value: 100, isActive: false },
+  { value: 5, isActive: true, type: "btn" },
+  { value: 10, isActive: false, type: "btn" },
+  { value: 20, isActive: false, type: "btn" },
+  { value: 30, isActive: false, type: "btn" },
+  { value: 50, isActive: false, type: "btn" },
+  { value: 100, isActive: false, type: "btn" },
+  { value: -1, isActive: false, type: "number" },
 ];
 
 type Action = { type: string; playload: number };
 
 export const switchButton = (state = initialState, action: Action) => {
+  console.log(action.type);
   switch (action.type) {
     case "SWITCH_MONEY_BUTTON":
       const newState = state.map((button: button) =>
         button.value === action.playload
-          ? { value: button.value, isActive: true }
-          : { value: button.value, isActive: false }
+          ? { value: button.value, isActive: true, type: button.type }
+          : { value: button.value, isActive: false, type: button.type }
       );
       return newState;
+
+    case "SET_NUMBER_VALUE":
+      return state.map((button: button) =>
+        button.type === "number"
+          ? { ...button, value: action.playload }
+          : { ...button }
+      );
+
+    case "ACTIVATE_NUMBER_BUTTON":
+      return state.map((button: button) =>
+        button.type === "number" ? { ...button, isActive: true } : { ...button }
+      );
+
+    case "DEACTIVATE_NUMBER_BUTTON":
+      return state.map((button: button) =>
+        button.type === "number"
+          ? { ...button, isActive: false }
+          : { ...button }
+      );
+
+    case "DEACTIVATE_BUTTONS":
+      return state.map((button: button) =>
+        button.type === "btn" ? { ...button, isActive: false } : { ...button }
+      );
+
     case "RESET":
       return initialState;
+
     default:
       return state;
   }
